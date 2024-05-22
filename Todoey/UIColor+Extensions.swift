2fea6ade81ef2dd692661d147339dc6ae9a23a6f
isAnastasia <nasty04012003@gmail.com>
2024-05-22 13:37:50 +0300

    colorTest

---
+++ Todoey/UIColor+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor+Extensions.swift
+//  Todoey
+//
+//  Created by Anastasia Gorbunova on 22.05.2024.
+//  Copyright © 2024 App Brewery. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static var backgroundColor: UIColor { UIColor(named: "color1") ?? UIColor.white}
+}
